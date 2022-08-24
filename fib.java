51008e96b0a7dd07fe4764d55ad20c2207dca5e7
Chobofied <TaylorJSouthworth@Gmail.com>
2022-08-24 16:10:01 -0700

    Working on Fib

---
+++ fib.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class fib {
+
+    public static void fiber(List<String> lister){
+
+        lister.get(1);
+        int x=5;
+    }
+    
+    public static void main(String[] args){
+        List<String> lister = new ArrayList<String>();
+        lister.add("1");
+        lister.add("1");
+        int[] mylist = {9,1,32,2,4,22};
+        lister.get(0);
+        fiber(lister);
+        System.out.println(lister);
+
+
+
+    }
+}
